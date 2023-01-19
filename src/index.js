b6c956e9ed6c08974e58a747a5ed60451284265c
stefanoHTB <danielelucca001@outlook.com>
2023-01-19 15:07:40 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import globalReducer from "./state/themeSlice";
+import { Provider } from 'react-redux';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from './state/api'
+
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducers = combineReducers({
+    global: globalReducer,
+    [api.reducerPath]: api.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+         <App /> 
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
